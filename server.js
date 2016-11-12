6624bbda50ccdaaa6b5f5b358535066124598b48
xeonjake <jiahaokuang7066@gmail.com>
2016-11-11 18:16:20 -0800

    added comments

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// temp vars, testing and development only
 var url = 'mongodb://localhost:27017';
 
 var mongo = require('mongodb').MongoClient;
@@ -11,20 +12,30 @@ var clientList = {};
 
 server.listen(80);
 
+// send the index.html when http request to /
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+    // log socket id that just connected
     console.log(socket.id);
+    
+    // add socket id to client list
     clientList[socket.id] = "";
+    // when get_socket_id event is received do ...
     socket.on('get_socket_id', function(data){
+        // log the data received
         console.log(data);
         // send private message
         io.to(socket.id).emit("message", socket.id);
     });
+    
+    // do ... when client disconnect
     socket.on('disconnect', function(){
         console.log("socket: "+socket.id+" disconnected");
+        
+        // remove the socket id from the client list
         delete clientList[socket.id];
     });
 });
